d75a1e33e70fbf255a05831871ee8d89fb357ef6
sarsdev <sarsdesenv@gmail.com>
2022-02-06 13:10:46 -0300

    Updating the Angular APP
    - Added loading page
    - Added data pagination
    - Added removal function
    - Changed table appearance
    - Advanced filter removed

--- projects/app/product-packaging-app/src/app/product-master-detail/product-master-detail.component.ts
+++ projects/app/product-packaging-app/src/app/product-master-detail/product-master-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PoPageFilter, PoTableColumn } from '@po-ui/ng-components';
+import { PoPageFilter, PoTableAction, PoTableColumn } from '@po-ui/ng-components';
 import { ProductService } from 'src/service/product.service';
 
 @Component({
@@ -10,19 +10,31 @@ import { ProductService } from 'src/service/product.service';
 export class ProductMasterDetailComponent implements OnInit {
 
   public pageFilter: PoPageFilter = {
-    action: this.quickFilter,
-    advancedAction: this.advancedFilter,
+    action: this.quickFilter.bind(this),
     placeholder: "Filtrar"
   };
 
   public tableColumns: Array<PoTableColumn> = [
-    { property: "id", visible: false },
-    { property: "code", label: "Code" },
-    { property: "description", label: "Description" },
-    { property: "unit", label: "Unit" }
+    { property: "id", width: "20%", type: "string", visible: false },
+    { property: "code", label: "Code", width: "15%", type: "number" },
+    { property: "description", label: "Description", width: "50%", type: "string" },
+    { property: "unit", label: "Unit", width: "20%", type: "string" }
   ];
 
-  public tableItems: any = [];
+  public tableActions: Array<PoTableAction> = [{
+    label: "Remove",
+    action: this.onDeleteTableRecord.bind(this),
+    icon: "po-icon po-icon-delete",
+    type: "danger"
+  }];
+
+  public qtdRecordsByPage: number = 10;
+  public qtdSkip: number = 0;
+  public loadingHidden: boolean = true;
+  public loadingActived: boolean = false;
+  public showMoreDisable: boolean = true;
+  public tableItems: Array<any> = [];
+  public tableItemsAuxiliary: Array<any> = [];
   public service: ProductService;
 
   constructor(pService: ProductService) { 
@@ -30,27 +42,67 @@ export class ProductMasterDetailComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.tableItems = this.getTableItems();
+    this.getTableItems();
   }
 
   public quickFilter(pFilter: string): void {
-    console.info(`quickFilter: ${pFilter}`);
+    if (pFilter === undefined || pFilter == "") {
+      this.tableItems = this.tableItemsAuxiliary;
+      return;
+    }
+    this.tableItems = this.tableItems.filter(f => f.code == pFilter || f.description == pFilter || f.unit == pFilter );
   }
 
-  public advancedFilter(): void {
-    console.info("advancedFilter");
+  public onShowMore(): void {
+    this.updatePagination();
+    this.getTableItems();
   }
 
-  private getTableItems(): void {
-    this.service.getTableItems().subscribe(
-      (data) => {
-        console.info(data);
-        this.tableItems = data.payload;
-      },
-      (err) => {
-        console.error(err);
-        this.tableItems = [];
-      }
-    );
+  public onDeleteTableRecord(record: any): void {
+    this.toggleLoading();
+    this.service.delTableItem(record.code)
+      .subscribe({
+        next: (data) => this.getTableItems(),
+        error: (err) => console.error(err),
+        complete: () => this.toggleLoading()
+      });
+  }
+
+  public getTableItems(): void {
+    this.toggleLoading();
+    this.service.getTableItems(this.getPagination())
+      .subscribe({
+        next: (data) => {
+          this.updateTableItems(data.payload);
+          this.setShowMore(data.footer.hasNext);
+        },
+        error: (err) => this.updateTableItems([]),
+        complete: () => this.toggleLoading()
+      });
+  }
+
+  private toggleLoading(): void {
+    this.loadingHidden = !this.loadingHidden;
+    this.loadingActived = !this.loadingActived;
+  }
+
+  private setShowMore(pHasNextData: boolean): void {
+    this.showMoreDisable = !pHasNextData;
+  }
+
+  private getPagination(): any {
+    return { 
+      qtdRecords: this.qtdRecordsByPage,
+      skip: this.qtdSkip 
+    };
+  }
+
+  private updatePagination(): void {
+    this.qtdSkip = this.tableItems.length;
+  }
+
+  private updateTableItems(pData: Array<any>) {
+    this.tableItems = this.tableItems.concat(pData);
+    this.tableItemsAuxiliary = this.tableItems;
   }
 }
--- projects/app/product-packaging-app/src/service/product.service.ts
+++ projects/app/product-packaging-app/src/service/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { first, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,17 @@ export class ProductService {
 
   constructor(private http: HttpClient) { }
 
-  public getTableItems(): Observable<any> {
-    return this.http.get<Observable<any>>("https://localhost:7053/ProductPackaging/product/packaging?qtdpage=10&skip=0", { observe: 'body', responseType: 'json', headers: new HttpHeaders("Access-Control-Allow-Origin: *") });
+  public getTableItems(pPagination: any): Observable<any> {
+    return this.http.get<Observable<any>>(
+      `https://localhost:7053/ProductPackaging/product/packaging?qtdpage=${pPagination.qtdRecords}&skip=${pPagination.skip}`,
+      { observe: 'body', responseType: 'json', headers: new HttpHeaders("Access-Control-Allow-Origin: *") }
+    ).pipe(first());
+  }
+
+  public delTableItem(pCode: number): Observable<any> {
+    return this.http.delete<Observable<any>>(
+      `https://localhost:7053/ProductPackaging/product/packaging/${pCode}`,
+      { observe: 'body', responseType: 'json', headers: new HttpHeaders("Access-Control-Allow-Origin: *") }
+    ).pipe(first());
   }
 }
